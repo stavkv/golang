e9270e77d147c3ad0abcb33b7ba8c4358fd10e95
mrvks <singhvikal891@gmail.com>
2023-11-13 15:41:01 +0530

    first commit

---
+++ value.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("go" + "lang")
+    
+    fmt.Println("vikal" + "singh")
+    fmt.Println("1+9 =", 1+9)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+    fmt.Println(!false)
+
+
+}
